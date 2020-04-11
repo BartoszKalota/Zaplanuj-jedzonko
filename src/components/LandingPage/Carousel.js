67329e8e782c476e3974167941ecad24acce4604
Bartosz Kalota <bartek.kalota@wp.pl>
2020-04-11 23:48:07 +0200

    Landing page carousel text content

--- src/components/LandingPage/Carousel.js
+++ src/components/LandingPage/Carousel.js
@@ -31,6 +31,17 @@ const Carousel = () => {
     }
   }, [index]);
   
+  let header = <h3>Lorem ipsum dolor sit amet</h3>;
+  let descr = <p>consectetur adipisicing elit. Obcaecati ipsam incidunt, corrupti nobis amet doloribus</p>
+  if (index === 1) {
+    header = <h3>Apple pie gingerbread halvah</h3>
+    descr = <p>macaroon cake pudding apple pie. Powder danish icing biscuit tart bonbon marzipan gummies sweet</p>
+  }
+  if (index === 2) {
+    header = <h3>Look at these words!</h3>
+    descr = <p>Are they small words? And he referred to my words - if they're small, something else must be small</p>
+  }
+
   return (
     <Row
       className='carousel-row'
@@ -40,7 +51,10 @@ const Carousel = () => {
         <div className='arrow' onClick={handleOnDecrement}>
           <i className="fas fa-chevron-left"></i>
         </div>
-        <h1>Carousel</h1>
+        <div className='text-container'>
+          {header}
+          {descr}
+        </div>
         <div className='arrow' onClick={handleOnIncrement}>
           <i className="fas fa-chevron-right"></i>
         </div>
