4b4692d487aeccdce841052b64c9f45425fcad52
Bartosz Kalota <bartek.kalota@wp.pl>
2020-04-15 17:10:59 +0200

    Landing page why us

--- src/components/LandingPage/CallToAction.js
+++ src/components/LandingPage/CallToAction.js
@@ -6,7 +6,7 @@ const CallToAction = () => (
     <Row className='call-to-action-row'>
       <Container className='call-to-action-container'>
         <div>
-          <h4>Lorem ipsum dolor sit amet</h4>
+          <h2>Lorem ipsum dolor sit amet</h2>
           <p>consectetur, adipisicing elit. Reiciendis et excepturi nulla repellat quisquam reprehenderit nihil aut distinctio tempore natus!</p>
         </div>
         <button>Lorem ipsum</button>
