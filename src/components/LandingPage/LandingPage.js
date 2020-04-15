a8ceba6dfc9cd855bec005d5c9d2ea761ef40c2c
Bartosz Kalota <bartek.kalota@wp.pl>
2020-04-15 23:38:00 +0200

    Landing page newsletter logic

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -10,29 +10,46 @@ import AboutMe from './AboutMe';
 import Contact from './Contact';
 import Footer from './Footer';
 
-const LandingPage = () => (
-  <>
-    <NavMenu />
-    <Carousel />
-    <CallToAction />
-    <WhyUs />
-    <Newsletter />
-    <Row className='about-me-row'>
-      <Container className='about-me-container' id='section-2'>
-        <AboutMe />
-      </Container>
-    </Row>
-    <Row className='contact-row'>
-      <Container className='contact-container' id='section-3'>
-        <Contact />
-      </Container>
-    </Row>
-    <Row className='footer-row'>
-      <Container className='footer-container'>
-        <Footer />
-      </Container>
-    </Row>
-  </>
-);
+const NEWSLETTERS_URL = 'http://localhost:3005/emailsForNewsletter';
+
+const LandingPage = () => {
+  // Zapisanie w bazie odbiorców newslettera adresu email użytkownika
+  const handleOnAddToNewsletter = (email) => {
+    fetch(NEWSLETTERS_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email
+      })
+    })
+  };
+
+  return (
+    <>
+      <NavMenu />
+      <Carousel />
+      <CallToAction />
+      <WhyUs />
+      <Newsletter onAddToNewsletter={handleOnAddToNewsletter} />
+      <Row className='about-me-row'>
+        <Container className='about-me-container' id='section-2'>
+          <AboutMe />
+        </Container>
+      </Row>
+      <Row className='contact-row'>
+        <Container className='contact-container' id='section-3'>
+          <Contact />
+        </Container>
+      </Row>
+      <Row className='footer-row'>
+        <Container className='footer-container'>
+          <Footer />
+        </Container>
+      </Row>
+    </>
+  );
+}
 
 export default LandingPage;
