da5b9814bbe4a8f8590fe640826604ad5a02c25e
Bartosz Kalota <bartek.kalota@wp.pl>
2020-04-23 12:53:05 +0200

    Part of preparation for JSS and material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import LandingPage from './components/LandingPage';
 import AppContainer from './components/AppMain/Signup';
-import Firebase, { FirebaseProvider } from './config/Firebase';
+import NotFound from './components/NotFound';
 
 const App = () => (
-  <FirebaseProvider value={Firebase}>
-    <AppContainer />
-  </FirebaseProvider>
+  <>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/app" component={AppContainer} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  </>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './scss/main.scss';
+// import './scss/main.scss';
 import App from './App';
 
 ReactDOM.render(
