a91539cd9c3d089c6f6bc95ba5bb1e67483651f7
Bartosz Kalota <bartek.kalota@wp.pl>
2020-05-08 23:49:24 +0200

    Desktop - Alerts

--- src/components/AppMain/Desktop.js
+++ src/components/AppMain/Desktop.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import {
   Grid,
   Typography
@@ -7,24 +8,40 @@ import {
 
 import * as ROUTES from '../../config/ROUTES';
 import TileButton from './elements/TileButton';
+import AlertInfo from './elements/AlertInfo';
+import AlertWarn from './elements/AlertWarn';
+import AlertSucc from './elements/AlertSucc';
 
-const Desktop = () => (
-  <>
-    <Grid container>
-      <Grid item container xs={12} md={5} wrap="nowrap" justify="space-evenly">
-        <Link to={ROUTES.RECEIPT}>
-          <TileButton btnTitle="dodaj przepis" />
-        </Link>
-        <Link to={ROUTES.SCHEDULE}>
-          <TileButton btnTitle="dodaj plan" />
-        </Link>
+const useStyles = makeStyles({
+  upperMainGrid: {
+    marginBottom: 10
+  }
+});
+
+const Desktop = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Grid container>
+        <Grid item container xs={12} md={5} wrap="nowrap" justify="space-evenly" className={classes.upperMainGrid}>
+          <Link to={ROUTES.RECEIPT}>
+            <TileButton btnTitle="dodaj przepis" />
+          </Link>
+          <Link to={ROUTES.SCHEDULE}>
+            <TileButton btnTitle="dodaj plan" />
+          </Link>
+        </Grid>
+        <Grid item container xs={12} md={7} className={classes.upperMainGrid}>
+          <AlertInfo />
+          <AlertWarn />
+          <AlertSucc />
+        </Grid>
       </Grid>
-      <Grid item container xs={12} md={7}>alert</Grid>
-    </Grid>
-    <Grid container style={{ marginTop: 20 }}>
-      <Typography variant="h3" component="h1">Desktop</Typography>
-    </Grid>
-  </>
-);
+      <Grid container>
+        <Typography variant="h3" component="h1">Desktop</Typography>
+      </Grid>
+    </>
+  );
+};
  
 export default Desktop;
---
+++ src/components/AppMain/elements/AlertSucc.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Alert } from '@material-ui/lab';
+import { Collapse, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+
+const AlertSucc = () => {
+  const [open, setOpen] = useState(true);     // docelowo zarządzanie odbędzie się przez context
+  const [displayValue, setDisplayValue] = useState(open);
+
+  // Dzięki temu, wraz z alertem zniknie div wrapujący ten alert i zwolni przestrzeń
+  // Opóźnienie ma na celu wyświetlenie pełnej animacji
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDisplayValue(open)
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [open]);
+
+  return (
+    <div style={{ width: '100%', display: displayValue ? 'block' : 'none' }}>
+      <Collapse in={open}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => setOpen(false)}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          severity="success"
+          variant="filled"
+          elevation={6}
+        >
+          Świetnie, że jesteś! Udanego planowania i smacznego! <EmojiEmotionsIcon style={{ display: 'inline', fontSize: '1.2rem' }} />
+        </Alert>
+      </Collapse>
+    </div>
+  );
+}
+ 
+export default AlertSucc;
