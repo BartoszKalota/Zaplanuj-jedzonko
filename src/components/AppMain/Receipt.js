01d8904000cb057c95eb3b9662f85d1edf6c083d
Bartosz Kalota <bartek.kalota@wp.pl>
2020-05-12 00:28:03 +0200

    Receipts - Appearance with random data

--- src/components/AppMain/Receipt.js
+++ src/components/AppMain/Receipt.js
@@ -1,9 +1,190 @@
-import React from "react";
-// import { withFirebaseHOC } from "../../config/Firebase";
+import React, { useContext } from "react";
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Button,
+  Divider,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from '@material-ui/core';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+
+import { DesktopSwitcher } from '../../config/contexts/DesktopSwitcher';
+
+import * as ROUTES from '../../config/ROUTES';
+
+const useStyles = makeStyles(theme => ({
+  heading: {
+    textAlign: 'left'
+  },
+  buttonContainer: {
+    justifyContent: 'flex-end'
+  },
+  addDataButton: {
+    minWidth: 'unset',
+    color: theme.palette.success.main,
+    padding: 0,
+    marginLeft: theme.spacing(1),
+    '& span svg': {
+      fontSize: '2.5rem'
+    }
+  },
+  table: {
+    height: 'calc(100vh - 220px)',
+    overflow: 'auto'
+  },
+  tableHeader: {
+    fontSize: '1.3rem',
+    textTransform: 'uppercase',
+    backgroundColor: theme.palette.background.paper
+  },
+  tableRow: {
+    '& td': {
+      fontSize: '0.95rem'
+    },
+    '&:nth-child(2n+1)': {
+      backgroundColor: 'rgba(0, 0, 0, 0.04)'
+    },
+    '&:hover': {
+      backgroundColor: 'rgba(0, 0, 0, 0.08)'
+    }
+  }
+}));
+
+// const StyledTableCell = withStyles(theme => ({
+//   head: {
+//     backgroundColor: theme.palette.common.black,
+//     color: theme.palette.common.white
+//   },
+//   body: {
+//     fontSize: 14
+//   }
+// }))(TableCell);
+// const StyledTableRow = withStyles(theme => ({
+//   root: {
+//     '&:nth-of-type(odd)': {
+//       backgroundColor: theme.palette.action.hover
+//     }
+//   }
+// }))(TableRow);
 
 const Receipt = () => {
-  return <h1>Receipt</h1>;
+  const classes = useStyles();
+  const history = useHistory();
+  const { setDesktopMode } = useContext(DesktopSwitcher);
+
+  const columns = [
+    { id: 'name', label: 'Name', minWidth: 170 },
+    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+    {
+      id: 'population',
+      label: 'Population',
+      minWidth: 170,
+      align: 'right',
+      format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+      id: 'size',
+      label: 'Size\u00a0(km\u00b2)',
+      minWidth: 170,
+      align: 'right',
+      format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+      id: 'density',
+      label: 'Density',
+      minWidth: 170,
+      align: 'right',
+      format: (value) => value.toFixed(2),
+    },
+  ];
+  function createData(name, code, population, size) {
+    const density = population / size;
+    return { name, code, population, size, density };
+  }
+  const rows = [
+    createData('India', 'IN', 1324171354, 3287263),
+    createData('China', 'CN', 1403500365, 9596961),
+    createData('Italy', 'IT', 60483973, 301340),
+    createData('United States', 'US', 327167434, 9833520),
+    createData('Canada', 'CA', 37602103, 9984670),
+    createData('Australia', 'AU', 25475400, 7692024),
+    createData('Germany', 'DE', 83019200, 357578),
+    createData('Ireland', 'IE', 4857000, 70273),
+    createData('Mexico', 'MX', 126577691, 1972550),
+    createData('Japan', 'JP', 126317000, 377973),
+    createData('France', 'FR', 67022000, 640679),
+    createData('United Kingdom', 'GB', 67545757, 242495),
+    createData('Russia', 'RU', 146793744, 17098246),
+    createData('Nigeria', 'NG', 200962417, 923768),
+    createData('Brazil', 'BR', 210147125, 8515767),
+  ];
+
+  const handleOnAddData = () => {
+    setDesktopMode(2);
+    history.push(ROUTES.DESKTOP);
+  };
+
+  return (
+    <Grid container direction="column">
+      <Grid item container>
+        <Grid item xs={6}>
+          <Typography className={classes.heading} variant="h5" component="h2" color="secondary">
+            LISTA PRZEPISÓW
+          </Typography>
+        </Grid>
+        <Grid item container className={classes.buttonContainer} xs={6}>
+          <Button className={classes.addDataButton} onClick={handleOnAddData}>
+            <AddBoxIcon />
+          </Button>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid item container>
+        <TableContainer component={Paper} className={classes.table}>
+          <Table stickyHeader aria-label="table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                    className={classes.tableHeader}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => {
+                return (
+                  <TableRow role="checkbox" tabIndex={-1} key={row.code} className={classes.tableRow}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number' ? column.format(value) : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
+  );
 };
 
-// export default withFirebaseHOC(Receipt);
 export default Receipt;
