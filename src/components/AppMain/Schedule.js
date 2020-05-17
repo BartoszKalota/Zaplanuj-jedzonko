bb3530434e47f7056e5d79a2b14659d40184eaa2
Bartosz Kalota <bartek.kalota@wp.pl>
2020-05-17 22:03:24 +0200

    Sorting functions in separated file

--- src/components/AppMain/Schedule.js
+++ src/components/AppMain/Schedule.js
@@ -27,6 +27,7 @@ import { IdClipboard } from '../../config/contexts/IdClipboard';
 import { DesktopSwitcher } from '../../config/contexts/DesktopSwitcher';
 
 import * as ROUTES from '../../config/ROUTES';
+import { getComparator, stableSort } from '../../config/sortingFunctions';
 import TableEditRemoveBtns from './elements/TableEditRemoveBtns';
 
 const useStyles = makeStyles(theme => ({
@@ -100,35 +101,6 @@ const useStyles = makeStyles(theme => ({
     }
   }
 }));
-
-// Funkcje sortowania
-const descendingComparator = (a, b, orderBy) => {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-};
-const getComparator = (order, orderBy) => {
-  return order === 'desc' ? (
-    (a, b) => descendingComparator(a, b, orderBy)
-  ) : (
-    (a, b) => -descendingComparator(a, b, orderBy)
-  );
-};
-const stableSort = (array, comparator) => {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map(el => el[0]);
-};
 
 // Kolumny
 const columns = [
