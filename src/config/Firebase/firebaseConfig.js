5c0758467b346db98b7b5da6e46914d9d86a5e9e
Bartosz Kalota <bartek.kalota@wp.pl>
2020-05-30 22:27:00 +0200

    Firebase config data moved to ENV constants

--- src/config/Firebase/firebaseConfig.js
+++ src/config/Firebase/firebaseConfig.js
@@ -1,9 +1,9 @@
 export default {
-  apiKey: "AIzaSyC3jmuIqis1-bubBm15hpQC0vV7BwTo-SA",
-  authDomain: "zaplanuj-jedzonko.firebaseapp.com",
-  databaseURL: "https://zaplanuj-jedzonko.firebaseio.com",
-  projectId: "zaplanuj-jedzonko",
-  storageBucket: "zaplanuj-jedzonko.appspot.com",
-  messagingSenderId: "386138684048",
-  appId: "1:386138684048:web:327d8de2a22e06cd62f615"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
